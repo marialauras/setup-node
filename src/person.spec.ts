e399cca05580046b12e49cb8832a4fdedb94ed40
Maria Laura <maria.raimundo@aluno.ufop.edu.br>
2023-09-27 16:25:20 -0300

    test: add person test

--- src/person.spec.ts
+++ src/person.spec.ts
@@ -1,6 +1,11 @@
 import Person from "@/person"
 
-it("should know my name", () => {
+it("should say hello", () => {
     const person = new Person()
     expect(person.sayHello()).toBe("hello world!")
+})
+
+it("should say bye", () => {
+    const person = new Person()
+    expect(person.sayBye()).toBe("bye world!")
 })
--- src/person.ts
+++ src/person.ts
@@ -2,6 +2,9 @@ class Person {
     sayHello() {
         return "hello world!"
     }
+    sayBye() {
+        return "bye world!"
+    }
 }
 
 export default Person
